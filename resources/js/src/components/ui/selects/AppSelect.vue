51eda8500cf73a9d91fea20257ed87d187df7d54
AlexRomanych <Alex.Romanych@yandex.ru>
2024-10-08 15:47:33 +0300

    fix:
    Создание backup

---
+++ resources/js/src/components/ui/selects/AppSelect.vue
@@ -0,0 +1,60 @@
+<template>
+
+<!--    <div v-if="selectData.data.length" :class="[width]"-->
+<!--         class="border-[2px] rounded-[6px] h-[58px] m-1 p-0 relative bg-slate-600">-->
+<!--        <label v-if="label"-->
+<!--               :for="selectData.name"-->
+<!--               class="absolute text-xs mt-1 ml-1 text-white font-semibold">-->
+<!--            {{ label }}-->
+<!--        </label>-->
+
+<!--        <select id="" :name="selectData.name" class="m-0 pt-4 h-full w-full rounded-[6px] bg-slate-600 text-white">-->
+
+<!--            <option v-for="item in selectData.data"-->
+<!--                    :key="item.id"-->
+<!--                    :value="item.id"-->
+<!--            >-->
+<!--                {{ item.name }}-->
+<!--            </option>-->
+
+<!--        </select>-->
+<!--    </div>-->
+</template>
+
+<script setup>
+import {colorsList} from "@/src/app/constants/colorsClasses.js"
+import {getColorClassByType, getTextColorClassByType} from "@/src/app/helpers/helpers.js"
+import {logicalExpression} from "@babel/types";
+
+const props = defineProps({
+    width: {
+        type: String,
+        required: false,
+        default: 'w-[300px]'
+    },
+    type: {
+        type: String,
+        required: false,
+        default: 'secondary',
+        validator: (type) => colorsList.includes(type)
+    },
+    selectData: {
+        type: Object,
+        required: false,
+        default: {name: Date.now().toString(), data: []},
+    },
+    label: {
+        type: String,
+        required: false,
+        default: 'Выберите...'
+    }
+
+})
+
+console.log(props.selectData)
+
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/src/views/TheMain.vue
+++ resources/js/src/views/TheMain.vue
@@ -3,7 +3,7 @@
         <div>The Main</div>
 
         <AppInputText
-            :id='newInput'
+            :id="newInput"
             :disabled="true"
             :type='"danger"'
             label='Введите имя'
@@ -11,76 +11,76 @@
         />
 
         <AppInputText
-            :id='newInput_1'
-            :error='workError'
-            :label='"Введите данные"'
-            :placeholder='"Enter..."'
-            :type='"warning"'
-            width='w-[300px]'
+            :id="newInput_1"
+            :error="workError"
+            label="Введите данные"
+            placeholder="Enter..."
+            type="warning"
+            width="w-[300px]"
         />
 
         <AppInputText
-            :id='newInput_2'
-            :label='"Введите данные"'
-            :placeholder='"Enter..."'
-            :type='"success"'
-            @getInputText='showText'
+            :id="newInput_2"
+            label="Введите данные"
+            placeholder="Enter..."
+            type="success"
+            @getInputText="showText"
         />
 
 
         <AppInputButton
-            :id='"newButton"'
-            title='Ошибка'
-            type='primary'
+            id="newButton"
+            title="Ошибка"
+            type="primary"
             @buttonClick="buttonClickHandler"
         />
 
         <AppInputButton
-            :id='"newButton"'
-            title='Ошибка'
-            type='secondary'
+            id="newButton"
+            title="Ошибка"
+            type="secondary"
             @buttonClick="buttonClickHandler"
         />
 
         <AppInputButton
-            :id='"newButton"'
-            title='Ошибка'
-            type='success'
+            id="newButton"
+            title="Ошибка"
+            type="success"
             @buttonClick="buttonClickHandler"
         />
 
         <AppInputButton
-            :id='"newButton"'
-            title='Ошибка'
-            type='danger'
+            id="newButton"
+            title="Ошибка"
+            type="danger"
             @buttonClick="buttonClickHandler"
         />
 
 
         <div>
             <AppInputButton
-                :id='"newButton"'
-                title='Ошибка'
-                type='info'
+                id="newButton"
+                title="Ошибка"
+                type="info"
                 @buttonClick="buttonClickHandler"
             />
 
             <AppInputButton
-                :id='"newButton"'
+                id="newButton"
                 title="Warning"
                 type="warning"
                 @buttonClick="buttonClickHandler"
             />
 
             <AppInputButton
-                :id='"newButton"'
+                id="newButton"
                 title="Ошибка"
-                type='light'
+                type="light"
                 @buttonClick="buttonClickHandler"
             />
 
             <AppInputButton
-                :id='"newButton"'
+                id="newButton"
                 title="Ошибка"
                 type="dark"
                 @buttonClick="buttonClickHandler"
@@ -93,11 +93,15 @@
         />
 
         <AppModal
-            text="A am modal"
             :show="false"
+            text="A am modal"
             type="info"
 
         />
+
+<!--        <AppSelect-->
+<!--            :selectData="selectData"-->
+<!--        />-->
 
 
     </main>
@@ -105,13 +109,13 @@
 
 
 <script setup>
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 import AppInputText from "@/src/components/ui/inputs/AppInputText.vue";
 import ErrorClass from "@/src/app/classes/ErrorClass.js";
 import AppInputButton from "@/src/components/ui/inputs/AppInputButton.vue";
 import AppCallout from "@/src/components/ui/callouts/AppCallout.vue";
 import AppModal from "@/src/components/ui/modals/AppModal.vue";
-
+import AppSelect from "@/src/components/ui/selects/AppSelect.vue";
 
 const newInput = ref('123')
 const newInput_1 = ref('123')
@@ -130,6 +134,23 @@ const buttonClickHandler = function (args) {
     console.log(args)
 }
 
+
+const selectData = {
+    name: 'numbers',
+    data: [
+        {id: 1, name: 'One'},
+        {id: 2, name: 'Two'},
+        {id: 3, name: 'Three', selected: true},
+        {id: 4, name: 'Four'},
+        {id: 5, name: 'Five'},
+    ]
+}
+
+
+// console.log(selectData)
+
+// const selectData = reactive({})
+
 </script>
 
 
@@ -138,6 +159,6 @@ const buttonClickHandler = function (args) {
     flex: 1;
     overflow-y: auto;
     margin: var(--header-height) 0 var(--footer-height) var(--sidebar-width);
- /*   position: relative; */
+    /*   position: relative; */
 }
 </style>
